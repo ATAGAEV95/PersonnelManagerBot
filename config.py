69072bfc6ee5109cd02f18be6d06a84f1d663cde
ATAGAEV <preator-man@mail.ru>
2025-04-24 18:55:01 +0300

    2025-04-20

--- config.py
+++ config.py
@@ -1,25 +1,37 @@
-TG_TOKEN = 'Ваш токен телеграмм бота'
+# TG_TOKEN = 'Ваш токен телеграмм бота'
 
 # Ваше подключение к БД Postgres
-DATABASE_URL = "postgresql+asyncpg://login:password@192.168.0.1:5432/bdname"
+# DATABASE_URL = "postgresql+asyncpg://login:password@192.168.0.1:5432/bdname"
 
 # Ваша схема из БД Postgres, по умолчанию "public"
-SCHEMA = "public"
+# SCHEMA = "public"
 
 # Ниже данные сервера где будут храниться фотографии пользователей,
 # в случае если вы не будете использовать этот функционал,
-# то закомментируйте код ниже
+# то удалите код ниже
 
 # Хост и порт сервера
-SSH_HOST = "192.168.0.1"
-SSH_PORT = 22
+# SSH_HOST = "192.168.0.1"
+# SSH_PORT = 22
 
 # Имя пользователя и пароль
-SSH_USERNAME = "root"
-SSH_PASSWORD = "12345678"
+# SSH_USERNAME = "root"
+# SSH_PASSWORD = "12345678"
 
 # Директория сервера куда будут сохраняться фотографии
-REMOTE_PATH = "/var/www/html/photo/"
+# REMOTE_PATH = "/var/www/html/photo/"
 
 # Адрес по которому можно будет открыть фотографию
-BASE_URL = "http://192.168.0.1/photo/"
+# BASE_URL = "http://192.168.0.1/photo/"
+
+import os
+
+TG_TOKEN = os.getenv("TG_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SCHEMA = "public"
+SSH_HOST = os.getenv("SSH_HOST")
+SSH_PORT = 22
+SSH_USERNAME = os.getenv("SSH_USERNAME")
+SSH_PASSWORD = os.getenv("SSH_PASSWORD")
+REMOTE_PATH = "/var/www/html/photo/"
+BASE_URL = os.getenv("BASE_URL")
